ece20d27dabac2bb97ee84863a577b0491088e27
Archie <110494298+archie122@users.noreply.github.com>
2023-06-15 10:41:26 -0600

    Code prep

--- code-wars-kotlin/src/main/kotlin/Main.kt
+++ code-wars-kotlin/src/main/kotlin/Main.kt
@@ -13,6 +13,7 @@ fun main(args: Array<String>) {
 //    println(Problem_14.plusOne(intArrayOf(1, 2, 3)))
 //    println(Problem_15.outed(mapOf("tim" to 0, "jim" to 2, "randy" to 0, "sandy" to 7, "andy" to 0, "katie" to 5, "laura" to 1, "saajid" to 2, "alex" to 3, "john" to 2, "mr" to 0), "laura"))
 //    println(Problem_16.printerError("kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"))
-    println(Problem_17.findSmallestInt(listOf(111111, 2222, 33, 4, 888, 545, 6, 7, 8, 999999)))
+//    println(Problem_17.findSmallestInt(listOf(111111, 2222, 33, 4, 888, 545, 6, 7, 8, 999999)))
+    println(Problem_18.lengthOfLastWord("Hello World"))
 
 }
---
+++ code-wars-kotlin/src/main/kotlin/Problem_18.kt
@@ -0,0 +1,5 @@
+object Problem_18 {
+    fun lengthOfLastWord(s: String): Int {
+        return s.split(" ").last().length
+    }
+}
